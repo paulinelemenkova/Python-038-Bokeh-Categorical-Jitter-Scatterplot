88539187a4211e8b8e32275fde7293bee7954a4f
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-21 09:21:17 +0800

    Add files via upload

--- Script-038b-Bokeh Categorical Jitter Scatterplot.py
+++ Script-038b-Bokeh Categorical Jitter Scatterplot.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[71]:
-
-
 import pandas as pd
 from matplotlib import pyplot as plt
 from bokeh.io import show, output_notebook
@@ -16,31 +12,24 @@ import os
 
 os.chdir('/Users/pauline/Documents/Python')
 df = pd.read_csv("Tab-Bathy.csv")
-df = df.melt(id_vars=['observ'], 
-              value_vars=["profile1", "profile2", "profile3", "profile4","profile5",
-            "profile6", "profile7","profile8", "profile9", "profile10",
-            "profile11", "profile12", "profile13", "profile14", "profile15",
-            "profile16", "profile17", "profile18", "profile19", "profile20",
-            "profile21", "profile22", "profile23", "profile24", "profile25"],
+
+# defining loop sequence
+profiles_nrs = list(map(lambda x: x, range(25)))
+profiles_list = []
+for i in profiles_nrs:
+    profiles_list.append('profile{}'.format(i + 1))
+
+df = df.melt(id_vars = ['observ'],
+             value_vars = profiles_list,
              var_name='profile', value_name='Depth')
 
-profile = ["profile1", "profile2", "profile3", "profile4","profile5",
-            "profile6", "profile7","profile8", "profile9", "profile10",
-            "profile11", "profile12", "profile13", "profile14", "profile15",
-            "profile16", "profile17", "profile18", "profile19", "profile20",
-            "profile21", "profile22", "profile23", "profile24", "profile25"]
+profile = profiles_list
 
 source = ColumnDataSource(df)
 
-#profile_cmap = factor_cmap('profile', palette=Spectral6,
- #                          factors=sorted(df.profile.unique()), start=1, end=25)
-
-p = figure(plot_width=800, plot_height=300, y_range=profile, 
+p = figure(plot_width=800, plot_height=300, y_range=profile,
            title="Categorical jitter plot of range of the bathymetric values,\n Mariana Trench, 25 profiles")
 
-#p.circle(x='Depth', y=jitter('profile', width=1.2, range=p.y_range), fill_color=profile_cmap, 
-#         source=source, alpha=0.3)
 p.circle(x='Depth', y=jitter('profile', width=1.2, range=p.y_range), source=source, alpha=0.3)
 
 show(p)
-
